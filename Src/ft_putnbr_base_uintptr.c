09b15234357b7c8b2c670947697edfd4fd9a5b4b
Brayan Saiago <90729742+brayans22@users.noreply.github.com>
2023-08-28 02:20:56 +0200

    Update ft_putnbr_base_uintptr.c

--- Src/ft_putnbr_base_uintptr.c
+++ Src/ft_putnbr_base_uintptr.c
@@ -3,10 +3,10 @@
 void ft_putnbr_base_uintptr(uintptr_t nb, uintptr_t base_number, char *base_content)
 {
     if (nb >= base_number)
-	{
-		ft_putnbr_base_uintptr(nb / base_number, base_number, base_content);
-		ft_putnbr_base_uintptr(nb % base_number, base_number, base_content);
-	}
-	else
-	    ft_putchar(base_content[nb]);
+    {
+	ft_putnbr_base_uintptr(nb / base_number, base_number, base_content);
+	ft_putnbr_base_uintptr(nb % base_number, base_number, base_content);
+    }
+    else
+	ft_putchar(base_content[nb]);
 }

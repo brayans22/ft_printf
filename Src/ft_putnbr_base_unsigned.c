edd1f5e459bf985f14088af11f96e4301e751d2a
Brayan Saiago <90729742+brayans22@users.noreply.github.com>
2023-09-26 18:22:28 +0200

    Delete Src/ft_putnbr_base_unsigned.c

--- Src/ft_putnbr_base_unsigned.c
+++
@@ -1,8 +0,0 @@
-#include "../Includes/util.h"
-
-void ft_putnbr_base_unsigned(unsigned int nb, unsigned int base_number, char *base_content)
-{
-    if (nb >= base_number)
-    	ft_putnbr_base_unsigned(nb / base_number, base_number, base_content);
-    ft_putchar(base_content[nb % base_number]);
-}
